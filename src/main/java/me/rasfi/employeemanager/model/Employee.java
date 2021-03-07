675daade703ecf582d091961761caf14cea5855f
Abdelhakim RASFI <7268784-Abdelhakim.rasfi@users.noreply.gitlab.com>
2021-03-08 00:56:47 +0100

    first commit

---
+++ src/main/java/me/rasfi/employeemanager/model/Employee.java
@@ -0,0 +1,10 @@
+package me.rasfi.employeemanager.model;
+
+public class Employee {
+    private long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+}
---
+++ src/test/java/me/rasfi/employeemanager/EmployeemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.rasfi.employeemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
