06ff7e972ed41b9bd3ae22ea2fbf178e37d8af1e
Abdelhakim RASFI <7268784-Abdelhakim.rasfi@users.noreply.gitlab.com>
2021-03-09 18:39:13 +0100

    Added: frontend folder, angularapp and the employee service

--- src/main/java/me/rasfi/employeemanager/repositories/EmployeeRepo.java
+++ src/main/java/me/rasfi/employeemanager/repositories/EmployeeRepo.java
@@ -2,12 +2,15 @@ package me.rasfi.employeemanager.repositories;
 
 import me.rasfi.employeemanager.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import java.util.Optional;
+
 
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
 
-    void deleteEmployeeById(Long id);
-
-    Optional<Employee> findEmployeeBydId(Long id);
+//    void deleteEmployeeById(Long id);
+//
+//    Optional<Employee> findEmployeeBydId(Long id);
 }
--- src/main/java/me/rasfi/employeemanager/service/EmployeeService.java
+++ src/main/java/me/rasfi/employeemanager/service/EmployeeService.java
@@ -3,15 +3,16 @@ package me.rasfi.employeemanager.service;
 import me.rasfi.employeemanager.exception.UserNotFoundException;
 import me.rasfi.employeemanager.model.Employee;
 import me.rasfi.employeemanager.repositories.EmployeeRepo;
-import net.bytebuddy.implementation.bytecode.Throw;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.nio.file.attribute.UserPrincipalNotFoundException;
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
+@Transactional
 public class EmployeeService {
     private final EmployeeRepo employeeRepo;
 
@@ -19,6 +20,8 @@ public class EmployeeService {
     public EmployeeService(EmployeeRepo employeeRepo) {
         this.employeeRepo = employeeRepo;
     }
+
+
 
     public Employee addEmployee(Employee employee){
         employee.setEmployeeCode(UUID.randomUUID().toString());
@@ -33,13 +36,13 @@ public class EmployeeService {
         return employeeRepo.save(employee);
     }
 
-    public void deleteEmployee(Long id)
-    {
-        employeeRepo.deleteEmployeeById(id);
-    }
-
-    public Employee findEmployeeById(Long id)
-    {
-        return employeeRepo.findEmployeeBydId(id).orElseThrow(() -> new UserNotFoundException("User by id " + id + " was not found."));
-    }
+//    public void deleteEmployee(Long id)
+//    {
+//        employeeRepo.deleteEmployeeById(id);
+//    }
+//
+//    public Employee findEmployeeById(Long id)
+//    {
+//       return employeeRepo.findEmployeeBydId(id).orElseThrow(() -> new UserNotFoundException("User by id " + id + " was not found."));
+//    }
 }
